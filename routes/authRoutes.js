7e3a91f971d8c6326e5a28394d798aa4daddf113
ChristianCaballero <christian.robles.info@gmail.com>
2025-11-21 20:15:05 -0600

    importando el middleware que verifica el token, y colocando las rutas

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,5 +3,18 @@
 const express = require('express')
 const router = express.Router() //creando el router 
 
-//importando los controllerss
+//importando los controllerss 
 const { register, login, getProfile } = require('../controllers/authController')
+
+//llamando al middleware de autenticacion de token
+const { verifyToken } = require('../middlewares/auth');
+
+//para las rutas públicas
+router.post('/register', register);
+router.post('/login', login);
+
+// para las rutas protegidas
+router.get('/profile', verifyToken, getProfile);
+
+//exportar el router
+module.exports = router;
