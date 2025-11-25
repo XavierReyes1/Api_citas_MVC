09c923133f7ae2bfebcd470e6a169478a901d4b2
ChristianCaballero <christian.robles.info@gmail.com>
2025-11-25 11:55:52 -0600

    añadiendo las rutas endpoints para los administradores y clientes

--- routes/citaRoutes.js
+++ routes/citaRoutes.js
@@ -12,3 +12,16 @@ const {
   getAllCitas,
   updateCitaStatus
 } = require('../controllers/citaController');
+
+const { verifyToken, isAdmin } = require('../middlewares/auth');
+
+// Añadiendo las rutas para clientes (autenticados)
+router.get('/', verifyToken, getMisCitas);
+router.post('/', verifyToken, createCita);
+router.delete('/:id', verifyToken, cancelarCita);
+
+// Añadiendo las rutas para administrador
+router.get('/admin/todas', verifyToken, isAdmin, getAllCitas);
+router.patch('/admin/:id/estado', verifyToken, isAdmin, updateCitaStatus);
+
+module.exports = router;
